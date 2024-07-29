cf6013c8493d98ccdae4039586d6c7f9913b6514
Shaily Jain <shailyjain0101@gmail.com>
2024-07-29 11:17:15 +0530

    add routes and gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'api/v1/scraping#index'
+
+  namespace :api do
+    namespace :v1 do
+      get 'scraping/scrape'
+      post 'scrape', to: 'scraping#scrape'
+    end
+  end
 end
